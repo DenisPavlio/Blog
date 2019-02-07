2579b040913744626951f494d7201689e810ae30
ndidenko <5773480@gmail.com>
2019-02-07 14:31:04 +0200

    rubocop style fixes for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,12 +8,16 @@ class Category < ApplicationRecord
 
   def name_rules
     splited = name.split
+    msg1 = 'Name must be at least 2 words'
+    msg2 = 'First and second words must be at least 2 letters'
+    msg3 = 'Name must be starts with a capital letter'
+    msg4 = 'Name must contain the dot character "."'
     if splited.size < 2
-      errors.add(:base, 'Name must be at least 2 words')
+      errors.add(:base, msg1)
     else
-      errors.add(:base, 'Name words must be at least 2 letters') if splited[0].size < 2 || splited[1].size < 2
+      errors.add(:base, msg2) if splited[0].size < 2 || splited[1].size < 2
     end
-    errors.add(:base, 'Name must be starts with a capital letter') unless name[0..0] =~ /[A-Z]/
-    errors.add(:base, 'Name must contain the dot character "."') unless name.include? '.'
+    errors.add(:base, msg3) unless name[0..0] =~ /[A-Z]/
+    errors.add(:base, msg4) unless name.include? '.'
   end
 end
